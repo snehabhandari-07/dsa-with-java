7793f00b59f10cd549ce98dd9f1e17ad65c3035d
snehabhandari-07 <snehabhandari013@gmail.com>
2025-08-02 14:59:59 +0530

    Added new files

---
+++ ApnaCollege/Readme.md
@@ -0,0 +1,6 @@
+# Apna College - DSA Course
+
+This folder contains my Java DSA solutions from the Apna College Course
+---
+
+Let’s keep the momentum going 🚀
---
+++ GFG-NationSkillUp/Maths&Combinators/EvenOdd.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void naiveApproach(int num) {
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    public static void evenOddEfficient(int num) {
+        // Bitwise AND
+        if ((num & 1) == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        naiveApproach(num);
+        evenOddEfficient(num);
+
+    }
+}
